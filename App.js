b0039ebebfcc2c8b083e8e66f98f35f824a84882
Shawn Kebel <fxrender@outlook.com>
2022-04-18 22:29:55 -0500

    :sparkles: Create `StackNavigator` function

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
@@ -6,16 +6,15 @@ import { useFonts } from 'expo-font';
 import Home from './screens/Home';
 import Details from './screens/Details';
 
-
-const Stack = createStackNavigator();
-
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     background: "transparent"
-  }
-}
+  },
+};
+
+const Stack = createStackNavigator();
 
 const App = () => {
   const [loaded] = useFonts({
@@ -30,14 +29,18 @@ const App = () => {
   
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}
-      initialRouteName="Home">
+      <Stack.Navigator 
+        screenOptions={{ 
+          headerShown: false 
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
 
